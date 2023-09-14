5c8cebf0f1b164d4efbece1e904e04cf0374d5ea
Chris Zheng <zhenchri@amazon.com>
2023-09-14 21:27:29 +0800

    testing upload

--- hybrid-cluster/lib/blueprints/hybrid.ts
+++ hybrid-cluster/lib/blueprints/hybrid.ts
@@ -1,11 +1,13 @@
 import { Construct } from 'constructs';
 import * as blueprints from '@aws-quickstart/eks-blueprints';
 import { KubevelaAddon } from '../addons/kubevela';
+import { GlobalResources } from '@aws-quickstart/eks-blueprints';
 
 export default class HybridCluster {
     static build(scope: Construct) {
         blueprints.EksBlueprint.builder()
             .version("auto")
+            .resourceProvider(GlobalResources.Vpc, new blueprints.VpcProvider(undefined, {primaryCidr: "10.10.0.0/16"}))
             .addOns(
                 new blueprints.AwsLoadBalancerControllerAddOn,
                 new blueprints.VpcCniAddOn, 
